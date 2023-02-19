65c76eaf54b5ba844ab11d3e1c3ab3bdd22765f1
NuminousBlue <jdiehl2236@gmail.com>
2023-02-19 14:02:49 -0600

    base

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+//
+//
+//
+mod command;
+use command::greet;
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+//
+//
+//
